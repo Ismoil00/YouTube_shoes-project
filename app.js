a48cd0e22655b19060132448c3ea89e0370c820e
test <juraev.abubakr_98@mail.ru>
2024-08-21 17:36:28 +0500

    done

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
+const modeSwitch = document.querySelector(".mode-switch");
+
+modeSwitch.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark");
+  modeSwitch.classList.toggle("active");
+});
+
 VanillaTilt.init(document.querySelectorAll(".box"), {
   max: 15,
   speed: 300,
   easing: "cubic-bezier(.03,.98,.52,.99)",
   scale: 1.1,
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  const modeSwitch = document.querySelector(".mode-switch");
-
-  modeSwitch.addEventListener("click", function () {
-    document.documentElement.classList.toggle("dark");
-    modeSwitch.classList.toggle("active");
-  });
 });
